3dba8f020c5cfd7eefa81737b7d17c2dbe848740
davide.cesari <davide.cesari@marconirovereto.it>
2026-02-26 10:40:29 +0100

    feat: implement BinaryTree and Node classes with traversal methods

--- depth-first/src/BinaryTree.java
+++ depth-first/src/BinaryTree.java
@@ -1,3 +1,50 @@
 public class BinaryTree {
+    private Node root; // head
+
+    public BinaryTree() {
+        this.root = null;
+    }
+
+    public BinaryTree(Node n) {
+        this.root = n;
+    }
     
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    // depth first
+    private void postOrder(Node n) {
+        // exit clause
+        if (n == null) return;
+
+        postOrder(n.getLeft());
+        postOrder(n.getRight());        
+        System.out.print(n.getData());
+    }
+
+    // in order
+    
+    /**
+     * I nodi genitori sono visitati prima dei figli
+     * @param n
+     */
+    private void preOrder(Node n) {
+        // exit clause
+        if (n == null) return;
+
+        System.out.print(n.getData());
+        preOrder(n.getLeft());
+        preOrder(n.getRight());
+    }
+
+    // post order
+    private void inOrder(Node n) {
+        // exit clause
+        if (n == null) return;
+
+        inOrder(n.getLeft());
+        System.out.print(n.getData());
+        inOrder(n.getRight());        
+    }
 }
